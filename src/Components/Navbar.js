89db1b584e176f7319210addca1675300e3f760e
Dhruv jindal <124202030+dhruvjindal555@users.noreply.github.com>
2024-11-01 23:46:18 +0530

    resume add

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion, useScroll, useSpring } from "framer-motion";
 function Navbar({portfolioRef,aboutRef,contactRef}) {
     const { scrollYProgress } = useScroll()
+    const navItemRef = useRef(null)
     const scaleX = useSpring(scrollYProgress)
     const scrollToPortfolio = () => portfolioRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })   
     const scrollToAbout = () => aboutRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })   
@@ -14,7 +15,7 @@ function Navbar({portfolioRef,aboutRef,contactRef}) {
                     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap ">DJ</span>
                     </a>
-                    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <button onClick={()=>{}} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
